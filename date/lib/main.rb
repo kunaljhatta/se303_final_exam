e15029be9161daf4afc47b11deaed365c79d4fdf
KunalJhatta <Kunaljhatta22@gmail.com>
2022-06-07 11:32:43 -0700

    refactored

--- date/lib/main.rb
+++ date/lib/main.rb
@@ -3,22 +3,44 @@
 #       refactoring.
 # Hint: Let the existing code use the new code.
 
+class Crop
+  attr_reader :name, :height
+  def initialize(name, height)
+    @name = name
+    @height = height
+  end
+end
+
+class Plot
+  attr_reader :crops, :name
+  def initialize(crops, name)
+    @crops = crops
+    @name = name
+  end
+
+  def size
+    crops.length
+  end
+
+  def total_height
+    crops.reduce(0) { |total, crop| total += crop.height }
+  end
+end
+
 class Farm
 
   attr_reader :plots
 
   def initialize
-    # Plants have a name and a height
-    corn = ["Sugar Baby", 3]
-    radish_1 = ["Red Boomer", 1]
-    radish_2 = ["Plump Pucker", 1]
-    cucumber = ["Green Lady", 2]
-    tomato = ["Dark Plump", 2]
-    beet_1 = ["Rainbow", 1]
-    beet_2 = ["Deep Darkness", 1]
-    # Plots have a name and some plants
-    plot_a = ["Plot A", corn, radish_1, radish_2]
-    plot_b = ["Plot B", cucumber, tomato, beet_1, beet_2]
+    corn = Crop.new("Sugar Baby", 3)
+    radish_1 = Crop.new("Red Boomer", 1)
+    radish_2 = Crop.new("Plump Pucker", 1)
+    cucumber = Crop.new("Green Lady", 2)
+    tomato = Crop.new("Dark Plump", 2)
+    beet_1 = Crop.new("Rainbow", 1)
+    beet_2 = Crop.new("Deep Darkness", 1)
+    plot_a = Plot.new([corn, radish_1, radish_2], "Plot A")
+    plot_b = Plot.new([cucumber, tomato, beet_1, beet_2], "Plot B")
     @plots = [plot_a, plot_b]
   end
 
@@ -27,19 +49,11 @@ class Farm
   end
 
   def total_number_of_plants
-                  # Subtract one from size because name is not a plant
-    plots.reduce(0) { |total, plot| total += (plot.size - 1) }
+    plots.reduce(0) { |total, plot| total += plot.size }
   end
 
   def total_plant_heights
-    total = 0
-    plots.each do |plot|
-      # Remove the plot name before iterating over the plants.
-      plot.drop(1).each do |plant|
-        total += plant[1]
-      end
-    end
-    total
+    plots.reduce(0) { |total, plot| total += plot.total_height}
   end
 
 end
